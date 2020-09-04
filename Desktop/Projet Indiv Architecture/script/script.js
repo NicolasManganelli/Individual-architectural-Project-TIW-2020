dca583b89d502a6540368b82ba9e9fdcc171ee0b
Nicolas Manganelli <nicolasmanganelli@iMac-de-Nicolas.local>
2020-09-04 09:39:46 +0200

    first commit

---
+++ Desktop/Projet Indiv Architecture/script/script.js
@@ -0,0 +1,20 @@
+
+
+var x = document.getElementById('mail');
+
+function request(){
+    let p = x.value;
+    var n = p.indexOf("@");
+    console.log(n);
+    if(p==0 || n == -1){
+        alert('Erreur, Rentrer votre adresse email ou completer avec des caractères spéciaux (\"@\").')
+    }else{
+       alert('Merci pour votre inscription, on se retrouve bientôt pour de nouvelle découverte musical !');
+        
+    }
+    
+}
+
+
+
+
