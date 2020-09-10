6855072faae19d4e3dbaebce3391c0e6c40e7633
Nicolas <manganelli.nico5@gmail.com>
2020-09-10 17:13:08 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,20 +1,26 @@
-
-
 var x = document.getElementById('mail');
 
-function request(){
+function request() {
     let p = x.value;
     var n = p.indexOf("@");
     console.log(n);
-    if(p==0 || n == -1){
+    if (p == 0 || n == -1) {
         alert('Erreur, Rentrer votre adresse email ou completer avec des caractères spéciaux (\"@\").')
-    }else{
-       alert('Merci pour votre inscription, on se retrouve bientôt pour de nouvelle découverte musical !');
-        
+    } else {
+        alert('Merci pour votre inscription, on se retrouve bientôt pour de nouvelle découverte musical !');
+
     }
-    
+
 }
 
 
 
-
+window.addEventListener("load", e => {
+    window.addEventListener("click", e => {
+        if (e.target.id == 'suite') {
+            console.log(e);
+            e.target.parentElement.classList.add("masque");
+            e.preventDefault();
+        }
+    });
+});
